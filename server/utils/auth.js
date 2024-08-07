cf0b7240b523617ba56e923a549e8dc3232efad5
EyeVorE <subplot_05tracer@icloud.com>
2024-08-07 16:22:09 -0500

    worked on server, auth, dependencies, etc

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,7 +6,7 @@ const secret = "mysecretsshhhhh";
 const expiration = "2h";
 
 module.exports = {
-  AuthentificationError: new GraphQLError("Could not authenticate user.", {
+  AuthenticationError: new GraphQLError("Could not authenticate user.", {
     extensions: {
       code: "UNAUTHENTICATED",
     },
